39fc315c3691e57cb56061406dda0c222f91e5e8
EwwaB <bania12311@gmail.com>
2019-02-13 19:17:19 +0100

    99 Bottles of beer

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,34 @@
 public class HelloWorld {
+	
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+	
+
+	 int liczbabutelek = 99; 
+	 String beer1 = "bottles of beer";
+
+	 while (liczbabutelek >0); {
+		 if ( liczbabutelek == 1 ) { beer1 = "bottle of beer";
+		 System.out.println (liczbabutelek + beer1 + "on the wall," + liczbabutelek + beer1 + 
+				 "Take one down and pass it around -" + liczbabutelek + beer1 + "on the wall.");
+		 
+	 }
+		 
+		 else {
+			 
+		            System.out.println( liczbabutelek + beer1 + "on the wall," + liczbabutelek + beer1 +
+		            		"Take one down and pass it around -" + liczbabutelek + beer1 + "on the wall.");
+		            }
+
+		            liczbabutelek = liczbabutelek - 1;
+
+
+
+		            if (liczbabutelek == 0){
+
+		                System.out.println( "no more bottles of beer on the wall" );
+
+		 }
+}
 	}
+	
 }
